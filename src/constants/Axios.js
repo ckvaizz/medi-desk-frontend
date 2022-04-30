381e819c74d5a9a6afea3a5f204770d683ee0ac5
ckvaizz <vaisakh.k591@gmail.com>
2022-04-30 21:56:51 +0530

    finished

--- src/constants/Axios.js
+++ src/constants/Axios.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 let user = JSON.parse(localStorage.getItem('user'))
 
 const instance = Axios.create({
- baseURL:"http://localhost:5000/api",
-  //  baseURL: "https://medi-desk-backend.herokuapp.com/api",
+//  baseURL:"http://localhost:5000/api",
+    baseURL: "https://medi-desk-backend.herokuapp.com/api",
   headers: {
     authorization: `Bearer ${user ? user.token : ""}`,
   },
